bbcbefdd4a1a17314a1546fbbce15aaff3e5e34b
abarhub <abarhub@gmail.com>
2023-01-09 08:23:50 +0100

    début implémentation

--- src/app.js
+++ src/app.js
@@ -2,8 +2,30 @@
 
 console.log('Hello world!');
 
+var board = null
+var game = new Chess()
+
+
+function makeRandomMove () {
+    var possibleMoves = game.moves()
+
+    // exit if the game is over
+    if (game.game_over()) return
+
+    var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+    game.move(possibleMoves[randomIdx])
+    board.position(game.fen())
+
+    window.setTimeout(makeRandomMove, 500)
+}
+
+
 var config = {
     pieceTheme: 'img/chesspieces/wikipedia/{piece}.png',
     position: 'start'
 }
-var board1 = Chessboard('board1', config)
+
+board = Chessboard('board1', config)
+
+
+window.setTimeout(makeRandomMove, 500)
